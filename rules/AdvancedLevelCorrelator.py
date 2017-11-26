72da54c104bbb4e7b1a1bac3eaaf4aef5310ffeb
Alessandro Fazio <agil3b3ast@hotmail.it>
2017-11-26 19:08:35 +0100

    Added categories option in AdvancedLevelCorrelator

--- rules/AdvancedLevelCorrelator.py
+++ rules/AdvancedLevelCorrelator.py
@@ -10,7 +10,14 @@ logger = log.getLogger(__name__)
 class ExtendedWindowHelper(WeakWindowHelper):
 
     def corrConditions(self):
-        alert_received = self.countAlertsReceivedInWindow()
+        ar = self.countAlertsReceivedInWindow()
+        if not ar:
+            logger.error("[%s] This Context Helper must have at least one category", self._name)
+            return False
+        alert_received = 0
+        for a in ar:
+            alert_received = alert_received + ar[a]
+        logger.debug("[%s] : alert received %s", self._name, alert_received, level=3)
         return alert_received >= self._ctx.getOptions()["threshold"]
 
 class AdvancedLevelCorrelator(Plugin):
@@ -33,7 +40,7 @@ class AdvancedLevelCorrelator(Plugin):
 
         if correlator.isEmpty():
 
-            options = { "expire": 30, "threshold": 5 ,"alert_on_expire": False, "window": 30, "reset_ctx_on_window_expiration": True, "check_burst": False }
+            options = { "expire": 30, "threshold": 5 ,"alert_on_expire": False, "window": 30, "reset_ctx_on_window_expiration": True, "check_burst": False, "categories" : ["my_category"] }
             initial_attrs = {"alert.correlation_alert.name": "Layer {} Correlation".format(LEVEL), "alert.classification.text": "MyFirstAdvancedLevelScan{}".format(NUMBER), "alert.assessment.impact.severity": "high"}
 
             correlator.bindContext(options, initial_attrs)
